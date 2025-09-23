d10f769d685dae42d958fd8b85e551616bc2ece7
PlamedieK <pkimpambudi@gmail.com>
2025-09-23 09:36:21 +0100

    feat:resolving the first exercice

--- 01-variables/exercice_2/main.js
+++ 01-variables/exercice_2/main.js
@@ -1,1 +1,3 @@
 //écrire votre code ici
+let kadea = "Dev Web et mobile"; // déclaration et initialisation de la variable
+console.log(kadea); // affichage  de la variable
--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,9 @@
 // Exercice 5
 // Écrivez votre code ici
+let temp = 28;
+
+let Fahrenheit = null;
+
+Fahrenheit = 28 * 9/5 + 32;
+
+console.log(Fahrenheit);
