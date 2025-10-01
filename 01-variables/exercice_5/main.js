964790857775d370b7ad779f6f8334f48e8d8a9d
PlamedieK <pkimpambudi@gmail.com>
2025-10-01 13:43:08 +0100

    fix:upgrade files

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,9 @@
 // Exercice 5
 // Écrivez votre code ici
+
+let celsius = 25; // Température en degrés Celsius
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32; 
+}
+
+console.log(celsiusToFahrenheit(celsius)); // Affiche la température en degrés Fahrenheit
--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,8 @@
 // Exercice 7
 // Écrivez votre code ici
+
+let ageEnfant = 14;
+let agePère = ageEnfant * 2;
+let ageMère = agePère-5;
+let ageGrandPère = ageMère*2 + ageEnfant/2;
+let ageOncle = ageGrandPère + 10; 
